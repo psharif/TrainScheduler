508d4ac25b5a71047942fe1878663be96145e5f5
psharif <psharif@asu.edu>
2018-02-01 21:48:24 -0800

    Added Timer Methods Needs some extra testing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,9 @@
 
   firebase.initializeApp(config);
 
-  var db = firebase.database();
+  var db = firebase.database(); 
+  var nextArrivalIntervalID; 
+  var minutesAwayIntervalID; 
 
   $("#submit-button").on("click", function(event){
 
@@ -19,31 +21,75 @@
   	var trainName = $("#train-name").val();
   	var departureStation = $("#departure-station").val();
   	var arrivalStation = $("#arrival-station").val();
+  	var firstTrainTime = $("#first-train-time").val();
   	var frequency = $("#frequency").val();
+
 
   	db.ref().push({
   		trainName : trainName, 
   		departureStation : departureStation,
   		arrivalStation : arrivalStation,
-  		frequency : frequency
+  		firstTrainTime : firstTrainTime,
+  		frequency : frequency 
   	});
- 
+  
   });
 
   db.ref().on("child_added", function(childSnapShot){
 
-  	console.log(childSnapShot.val()); 
+  	console.log(childSnapShot.key); 
 
   	var newRow = $("<tr>").html("<td>" + childSnapShot.val().trainName +
   		"</td><td>" + childSnapShot.val().departureStation +"</td><td>" + 
   		childSnapShot.val().arrivalStation +"</td><td>" + 
-  		childSnapShot.val().frequency +"</td><td>" + "placeHolder For Next Arrival" +
-  		"</td><td>" + "placeHolder For Minutes Away" +"</td>");
+  		childSnapShot.val().frequency +"</td><td id='" + childSnapShot.key + 
+  		"next-arrival'></td><td id='" + childSnapShot.key + "minutes-away'></td>");
 
-  	$("#schedule-table").append(newRow);
+  	$("#schedule-table").append(newRow); 
+
+  	minutesAwayIntervalID = setInterval(function(){
+  		var minutesLeft = calculateMinutesLeft(childSnapShot.val().frequency, childSnapShot.val().firstTrainTime);
+  		$("#" + childSnapShot.key + "minutes-away").text(minutesLeft + " mins");
+  		$("#" + childSnapShot.key + "next-arrival").text(minutesTillNextTrain(minutesLeft));
+  	} , 30 *1000);
 
   }, function(errorObject){
   	 console.log("Errors handled: " + errorObject.code);
   });
+
+
+
+  function minutesTillNextTrain(minutesLeft){
+  	var nextTrain = moment().add(minutesLeft, "minutes");
+    return moment(nextTrain).format("hh:mm");
+  }
+
+
+  function calculateMinutesLeft(frequency, firstTrainTime){
+
+  	var tFrequency = frequency;
+
+    var firstTrainTime = firstTrainTime;
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTrainTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTrainTimeConverted), "minutes");
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
+    return tMinutesTillTrain;
+
+  }
+
+
 
 alert("working");
